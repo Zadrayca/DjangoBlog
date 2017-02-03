fbcc372899edd9e210686be7728588a98a1b0d7e
Zadrayca <coxatiy@gmail.com>
2017-02-03 22:25:13 +0300

    02.03.2017

---
+++ DjangoRest/blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db.models import CharField, TextField, DateTimeField, ImageField
+
+
+class Post(models.Model):
+    title = CharField(max_length=100)
+    text = TextField()
+    image = ImageField(upload_to="images/%Y/%m/%d")
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
---
+++ DjangoRest/blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'blog'
+urlpatterns = [
+    url(r'^posts/$', views.posts, name='posts'),
+    url(r'^post/(?P<post_id>[0-9]+)/$', views.post, name='post'),
+    url(r'^login/$', views.login_view, name='login'),
+    url(r'^api/posts/$', views.api_post, name='api_post'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
